f0fe54a19f0d2b4322f1fd4f7feb810e3a71fab7
akashmudubagilu <akashmudubagilu@gmail.com>
2013-07-15 16:02:58 -0700

    Initial Commit

---
+++ MarkitPlayGround/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MarkitPlayGround
+//
+//  Created by Akash Mudubagilu on 7/15/13.
+//  Copyright (c) 2013 Akash Mudubagilu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
